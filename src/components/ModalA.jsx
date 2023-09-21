aa8dda3c782c648d508763bc8e31ea66d0bdd4a2
John Doe <mohaiminur24@gmail.com>
2023-09-21 20:09:23 +0600

    problem B modal design is complete

---
+++ src/components/ModalA.jsx
@@ -0,0 +1,104 @@
+import React, { useEffect } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+
+const ModalA = ({ isOpenModalA, setIsOpenModalA, OpenModalB }) => {
+  const [data, setData] = useState(null);
+
+  function closeModal() {
+    setIsOpenModalA(false);
+  }
+
+  function openModal() {
+    setIsOpenModalA(true);
+  }
+
+//   fetch data from api
+useEffect(()=>{
+    fetch("https://contact.mediusware.com/api-doc/?format=openapi").then(res=> res.json())
+    .then(data=> console.log(data));
+},[])
+
+  return (
+    <>
+      <Transition appear show={isOpenModalA} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    All Contacts Modal
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500">
+                      Your payment has been successfully submitted. We’ve sent
+                      you an email with all of the details of your order.
+                    </p>
+                  </div>
+
+                  <div className="mt-4 space-x-1">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-[#46139f] px-4 py-2 text-sm font-medium text-white hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={openModal}
+                    >
+                      switch Modal A
+                    </button>
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-[#ff7f50] px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={() => {
+                        closeModal();
+                        OpenModalB();
+                      }}
+                    >
+                      switch Modal B
+                    </button>
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-[#46139f] px-4 py-2 text-sm font-medium text-white hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={closeModal}
+                    >
+                      close
+                    </button>
+                  </div>
+
+                  <div className="text-sm space-x-3 my-2">
+                    <input type="checkbox" />
+                    <label htmlFor="">Only even</label>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+};
+
+export default ModalA;
--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, { useState } from "react";
+import ModalA from "./ModalA";
+import ModalB from "./ModalB";
 
 const Problem2 = () => {
+  let [isOpenModalA, setIsOpenModalA] = useState(false);
+  let [isOpenModalB, setIsOpenModalB] = useState(false);
 
-    return (
+//   open modal B
+const OpenModalB = () =>{
+    setIsOpenModalB(true)
+}
+const OpenModalA =()=>{
+    setIsOpenModalA(true);
+}
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
-                <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
-                </div>
-                
-            </div>
+  return (
+    <>
+      <div className="container">
+        <div className="row justify-content-center mt-5">
+          <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
+
+          <div className="d-flex justify-content-center gap-3">
+            <button
+              onClick={() => setIsOpenModalA(true)}
+              className="btn btn-lg btn-outline-primary"
+              type="button"
+            >
+              All Contacts
+            </button>
+            <button
+              onClick={() => setIsOpenModalB(true)}
+              className="btn btn-lg btn-outline-warning"
+              type="button"
+            >
+              US Contacts
+            </button>
+          </div>
         </div>
-    );
+      </div>
+      {/* Modal A is here */}
+      <ModalA isOpenModalA={isOpenModalA} setIsOpenModalA={setIsOpenModalA} OpenModalB={OpenModalB}/>
+
+      {/* Modal A is here */}
+      <ModalB isOpenModalB={isOpenModalB} setIsOpenModalB={setIsOpenModalB} OpenModalA={OpenModalA}/>
+    </>
+  );
 };
 
 export default Problem2;
